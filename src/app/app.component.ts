43a01c22d676ecc5dacc201d819354ae982f7d61
Agnihotri <shubham.agnihotri@invenco.com>
2025-03-11 00:19:36 +0530

    app component updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'basic-ng-fe-app';
+  title = 'hello basic-ng-fe-app';
 }
