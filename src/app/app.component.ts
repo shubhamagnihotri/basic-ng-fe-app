01802b939026356da1ff371e1996ebfea3129c5e
Agnihotri <shubham.agnihotri@invenco.com>
2025-03-11 22:35:13 +0530

    app component updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hello shubham again updated shubham basic-ng-fe-app';
+  title = 'hello shubham agnihotri  again updated shubham basic-ng-fe-app';
 }
